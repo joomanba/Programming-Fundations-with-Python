39e2221277d07c6f95b5e13db716c58c78fdff06
joomanba <joomanba@gmail.com>
2016-03-21 16:11:40 +0900

    꽃모양을 만들기 위해 20도씩 18번을 오른편으로 회전하면서 사각형을 그림

--- 02.mindstorms.py
+++ 02.mindstorms.py
@@ -1,27 +1,33 @@
 import turtle
 
-def draw_square(some_turtle) :
-    for i in range(1, 5) :
+
+def draw_square(some_turtle):
+    for i in range(1, 5):
         some_turtle.forward(100)
         some_turtle.right(90)
 
-def draw_art() :
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
 
-    # Create the turtle Brad - Draw a square
+    # Create the turtle Brad - Draw a flower
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    draw_square(brad)
 
-    # Create the turtle Angie - Draws a circle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
+    for i in range(1, 19):
+        draw_square(brad)
+        brad.right(20)
+
+        # Create the turtle Angie - Draws a circle
+    #    angie = turtle.Turtle()
+    #    angie.shape("arrow")
+    #    angie.color("blue")
+    #    angie.circle(100)
 
     window.exitonclick()
+
 
 draw_art()
