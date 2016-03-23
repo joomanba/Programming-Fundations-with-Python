a523d64473330fcea954c32664b34ead2708efee
joomanba <joomanba@gmail.com>
2016-03-23 18:06:40 +0900

    결과값을 이해하기 쉽도록 변경

--- 2c.profanity_check/profanity_check.py
+++ 2c.profanity_check/profanity_check.py
@@ -17,5 +17,12 @@ def check_profanity(text_to_check):
     print(output)
     connection.close()
 
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("This document has no curse words")
+    else:
+        print("Could not scan the document properly")
+
 
 read_text()
