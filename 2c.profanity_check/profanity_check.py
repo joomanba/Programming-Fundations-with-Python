4de306c52332a7ebaae5cf201800554239014a35
joomanba <joomanba@gmail.com>
2016-03-23 17:48:46 +0900

    비속어를 판별하는 프로그램 개발 2가지 이슈가 있음 1. 비속어 판별 서비스 Deprecated 되어 아래 서비스 활용 "http://www.wdylike.appspot.com/?q=" + text_to_check
    2. 소문자만 분석 가능하여 Shit을 shit으로 변경함

---
+++ 2c.profanity_check/profanity_check.py
@@ -0,0 +1,18 @@
+import urllib
+
+def read_text():
+    # open - Python Built-in Function
+    # https://docs.python.org/2/library/functions.html#open
+    quotes = open("/Users/euijin/dev/python/Programming Fundations with Python/2c.profanity_check/movie_quotes.txt")
+    contents_of_files = quotes.read()
+    print(contents_of_files)
+    quotes.close()
+    check_profanity(contents_of_files)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+
+read_text()
